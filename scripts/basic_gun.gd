08f2dd58a6ca7b3dd9f91a74e9f8a88f72bd7c3d
JM <somethingmsb@email.com>
2024-01-02 18:44:31 -0500

    drift refactor

--- scripts/basic_gun.gd
+++ scripts/basic_gun.gd
@@ -6,8 +6,8 @@ const COOLDOWN := .11
 const DAMAGE := 2
 const PIERCE := .1
 
-const MAX_DRIFT := 1500
-var drift := 0
+const MAX_DRIFT := 15.0
+var drift := .0
 
 func _process(_delta):
-	drift = clamp(drift - 1, 0, MAX_DRIFT)
+	drift = clamp(drift - .035, 0, MAX_DRIFT)
--- scripts/basic_gun_bullet.gd
+++ scripts/basic_gun_bullet.gd
@@ -8,7 +8,5 @@ func fire(goalPos, startPos, weaponDamage, pierce):
 	var shot = Projectile.instantiate()
 	add_child(shot)
 	shot.fire(goalPos, startPos, weaponDamage, pierce)
-	var rando : int = 0 if gun.drift == 0 else randi() % gun.drift
-	var dir : int = 1 if randi() % 2 == 1 else -1
-	shot.rotate_direction(rando * dir / 100)
-	gun.drift += 100
+	shot.rotate_direction(randf_range(gun.drift * -1, gun.drift))
+	gun.drift += 1
